797672682f2223f572fb597d2e9a03f0c3a42fb7
moribus-single <dvg37@tpu.ru>
2022-08-21 00:33:41 +0700

    add some writers

---
+++ writers/csv_writer.py
@@ -0,0 +1,20 @@
+from base_writer import BaseWriter
+import csv
+
+
+class CsvWriter(BaseWriter):
+    def __init__(self, data: list, output_path: str = ""):
+        super().__init__(
+            data=data,
+            output_path=output_path,
+            output_type="csv"
+        )
+
+    def write(self, titles: list, keys: list):
+        with open(self.output_path, 'w', newline='', encoding="utf-8") as file:
+            writer = csv.writer(file, delimiter=',')
+            writer.writerow(titles)
+
+            for user in self.data:
+                dump_list = self._prepare_list(user, keys)
+                writer.writerow(dump_list)
